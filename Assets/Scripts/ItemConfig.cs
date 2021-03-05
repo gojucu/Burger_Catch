451395001545616760a6d2808318a67c3d8ee062
FurkanÇetinkaya <gojucu@gmail.com>
2021-03-06 01:06:04 +0300

    yerleştirme kodları yazıldı denemelik bitki oluşturulup denenicek

---
+++ Assets/Scripts/ItemConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("Item"))]
+public class ItemConfig : ScriptableObject
+{
+    public Transform placeTransform;
+    [SerializeField] GameObject itemPrefab;
+
+    public GameObject GetItemPrefab()
+    {
+        return itemPrefab;
+    }
+}
---
+++ Assets/Scripts/ItemPlacementSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlacementSystem : MonoBehaviour
+{
+    //Her kategori için bunu ayrı yazmam gerekebilir** Plantla başla örneğe
+    [SerializeField] ItemConfig currentItemConfig = null;//Plant olucak ilk
+
+    GameObject itemObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PutItemInPosition(currentItemConfig);
+    }
+
+    public void PutItemInPosition(ItemConfig itemToPlace)
+    {
+        currentItemConfig = itemToPlace;
+        var itemPrefab = itemToPlace.GetItemPrefab();
+        Destroy(itemObject);
+        itemObject = Instantiate(itemPrefab, this.transform);
+        itemObject.transform.localPosition = currentItemConfig.placeTransform.localPosition;
+        itemObject.transform.localRotation = currentItemConfig.placeTransform.localRotation;
+    }
+
+
+}
